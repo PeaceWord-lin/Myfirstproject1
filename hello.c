f8d116699169997734872078d736cdc3da8cab73
lin <linshihe25@gmail.com>
2021-01-27 18:03:39 +0000

    repair format

--- hello.c
+++ hello.c
@@ -1,15 +1,7 @@
-{\rtf1\ansi\ansicpg936\cocoartf2577
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 #include<studi.h>\
-\
-Int main()\
-\{\
-	print(\'91hello world\'92);\
-	return 0;\
-\
-\}}
+int main()
+{
+	print('hello world');
+	return 0;
+
+}
